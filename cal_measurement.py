6360a8a27233ea8c0770f739820b0db2830a6fab
YYu HHwan KKim <31001511+Kimyuhwanpeter@users.noreply.github.com>
2021-09-30 19:23:49 +0900

    Update cal_measurement.py

--- cal_measurement.py
+++ cal_measurement.py
@@ -3,31 +3,36 @@ import tensorflow as tf
 import numpy as np
 import os
 
-total_classes = 96  # 0 라벨이 void 처리된 것
+class Measurement:
+    def __init__(self, predict, label, shape, total_classes=96):
+        self.predict = predict
+        self.label = label
+        self.total_classes = total_classes
+        self.shape = shape
 
-def MIOU(predict, label, total_classes, shape=[720*1280,]):
+    def MIOU(self):
 
-    predict_ = np.reshape(predict, shape)
-    label_ = np.reshape(label, shape)
+        self.predict = np.reshape(self.predict, self.shape)
+        self.label = np.reshape(self.label, self.shape)
 
-    predict_count = np.bincount(predict_, minlength=total_classes)
-    predict_count = np.delete(predict_count, 0) # delete 0 label (void class)
-    label_count = np.bincount(label_, minlength=total_classes)
-    label_count = np.delete(label_count, 0)  # delete 0 label (void class)
+        predict_count = np.bincount(self.predict, minlength=self.total_classes)
+        predict_count = np.delete(predict_count, -1) # delete last label (void class)
+        label_count = np.bincount(self.label, minlength=self.total_classes)
+        label_count = np.delete(label_count, -1)  # delete last label (void class)
 
-    temp = total_classes * label_ + predict_  # Get category metrics
+        temp = self.total_classes * np.array(self.label, dtype="int") + np.array(self.predict, dtype="int")  # Get category metrics
     
-    temp_count = np.bincount(temp, minlength=total_classes*total_classes)
-    cm = np.reshape(temp_count, [total_classes, total_classes])
-    cm = np.diag(cm)
-    cm = np.delete(cm, 0)   # delete 0 label (void class)
+        temp_count = np.bincount(temp, minlength=self.total_classes*self.total_classes)
+        cm = np.reshape(temp_count, [self.total_classes, self.total_classes])
+        cm = np.diag(cm)
+        cm = np.delete(cm, -1)   # delete last label (void class)
     
-    U = label_count + predict_count - cm
+        U = label_count + predict_count - cm
 
-    miou = cm / U
-    miou = np.nanmean(miou)
+        miou = cm / U
+        miou = np.nanmean(miou)
 
-    return miou
+        return miou
 
 
 #import matplotlib.pyplot as plt
@@ -35,11 +40,11 @@ def MIOU(predict, label, total_classes, shape=[720*1280,]):
 #if __name__ == "__main__":
 
     
-#    path = os.listdir("D:/[1]DB/[5]4th_paper_DB/Fruit/MinneApple/detection/train/masks")
+#    path = os.listdir("D:/[1]DB/[5]4th_paper_DB/other/CamVidtwofold_gray/CamVidtwofold_gray/train/labels")
 
 #    b_buf = []
 #    for i in range(len(path)):
-#        img = tf.io.read_file("D:/[1]DB/[5]4th_paper_DB/Fruit/MinneApple/detection/train/masks/"+ path[i])
+#        img = tf.io.read_file("D:/[1]DB/[5]4th_paper_DB/other/CamVidtwofold_gray/CamVidtwofold_gray/train/labels/"+ path[i])
 #        img = tf.image.decode_png(img, 1)
 #        img = tf.image.resize(img, [513, 513], method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
 #        img = tf.image.convert_image_dtype(img, tf.uint8)
@@ -54,7 +59,11 @@ def MIOU(predict, label, total_classes, shape=[720*1280,]):
 #        b_buf.append(len(b))
 #        total_classes = len(b)  # 현재 124가 가장 많은 클래스수
 
-#        miou = MIOU(predict=img, label=img, total_classes=total_classes, shape=[img.shape[0]*img.shape[1],])
-#        print(miou)
+#        #miou = MIOU(predict=img, label=img, total_classes=total_classes, shape=[img.shape[0]*img.shape[1],])
+#        miou_ = Measurement(predict=img,
+#                            label=img, 
+#                            shape=[513*513, ], 
+#                            total_classes=12).MIOU()
+#        print(miou_)
 
 #    print(np.max(b_buf))
